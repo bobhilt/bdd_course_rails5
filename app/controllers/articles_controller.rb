3c9d726dd8b21b4ed9753abc83f3feeb4226d84b
Bob Hiltner <bobhilt@gmail.com>
2016-11-12 00:00:28 -0800

    Set up user sign-in.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+#  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles=Article.all
@@ -57,5 +58,9 @@ class ArticlesController < ApplicationController
     end
     def set_article
       @article = Article.find(params[:id])
+    end
+    
+    def authenticate_user
+
     end
 end
